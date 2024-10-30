f9d07e0f10871babb660af0ff1d14bce8836d1ab
digimodify <chrisetienne1@gmail.com>
2024-10-30 10:47:29 -0400

    Update hello

--- hello/hello.go
+++ hello/hello.go
@@ -1,11 +1,10 @@
 package main
 
-import "fmt"
+const englishHelloPrefix = "Hello, "
 
 func Hello(name string) string {
-	return "Hello, "+ name
-}
-
-func main() {
-	fmt.Println(Hello("world"))
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
